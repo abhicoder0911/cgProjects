612963eb7921aaed8c45e5e06dc2453e8e19fa3f
abhicoder0911 <siri.munni@gmail.com>
2025-05-21 09:49:25 +0530

    Add files via upload

---
+++ activity/functions.py
@@ -0,0 +1,12 @@
+def well_wishes():
+    print("hello")
+    print("how are you")
+
+#well_wishes()
+
+def weather_conditions():
+    print("weather is pleasant in:",spring)
+    print("the weather is same in ",autumn)
+spring="autumn"
+autumn=spring
+weather_conditions()
